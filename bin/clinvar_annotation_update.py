6f1f69611cf45ce5b23bf765847f0682cbe1f484
Polaris Astrum <polaris.astrum@nhs.net>
2024-06-27 15:56:09 +0100

    Added argparse, config file check, comments, minor changes

--- bin/clinvar_annotation_update.py
+++ bin/clinvar_annotation_update.py
@@ -2,8 +2,8 @@
 Runs Phoenix ClinVar annotation resource update
 """
 
+import argparse
 import json
-import sys
 from clinvar_file_fetcher import connect_to_website
 
 
@@ -17,8 +17,10 @@ def run_annotation_update(config_path) -> None:
     clinvar_base_link, clinvar_link_path = load_config(config_path)
     ftp = connect_to_website(clinvar_base_link, clinvar_link_path)
 
+    # for all files returned by ftp, add names to file_list
     file_list = []
     ftp.retrlines('LIST', file_list.append)
+
     for file in file_list:
         print(file)
     print("Exiting Phoenix")
@@ -36,18 +38,26 @@ def load_config(config_path):
     """
     with open(config_path, "r", encoding="utf8") as json_file:
         config = json.load(json_file)
+    keys = [
+        "CLINVAR_BASE_LINK",
+        "CLINVAR_LINK_PATH_B38"
+    ]
+    try:
+        assert keys in config
+    except AssertionError:
+        raise RuntimeError(
+            "Config file does not contain expected keys"
+        )
     clinvar_base_link = config.get("CLINVAR_BASE_LINK")
     clinvar_link_path = config.get("CLINVAR_LINK_PATH_B38")
     return clinvar_base_link, clinvar_link_path
 
 
 if __name__ == "__main__":
-    # validate arguments
-    num_args = len(sys.argv)
-    if num_args < 2:
-        raise RuntimeError(
-            "Error: path to config file must be specified when running"
-            + "clinvar_annotation_update.py"
-        )
+    parser = argparse.ArgumentParser()
+    # Add arguments
+    parser.add_argument('--config_file', type=str, required=True)
+    # Parse arguments
+    args = parser.parse_args()
 
-    run_annotation_update(sys.argv[1])
+    run_annotation_update(args.config_file)
