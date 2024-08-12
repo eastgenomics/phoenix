204b617e17d5a7c05b6ee63f32510745e67e7d73
Polaris Astrum <polaris.astrum@nhs.net>
2024-08-12 15:11:30 +0100

    Added unit tests

--- tests/test_clinvar_file_fetcher.py
+++ tests/test_clinvar_file_fetcher.py
@@ -13,14 +13,30 @@ from unittest.mock import Mock, patch, mock_open
 
 
 class TestClinvarFileFetcher(unittest.TestCase):
-    def test_connect_to_website(self):
-        pass
+    @patch("bin.clinvar_file_fetcher.time.sleep")
+    @patch("bin.clinvar_file_fetcher.FTP")
+    def test_connect_to_website(self, mock_ftp, mock_sleep):
+        assert connect_to_website(
+            "https://ftp.ncbi.nlm.nih.gov",
+            "/pub/clinvar/vcf_GRCh38/weekly/file.txt"
+        ) == mock_ftp.return_value
 
-    def test_get_most_recent_clivar_file_info(self):
-        pass
-
-    def test_download_clinvar_dnanexus(self):
-        pass
+    @patch("bin.clinvar_file_fetcher.download_file_upload_DNAnexus")
+    def test_download_clinvar_dnanexus(self, mock_download):
+        """Test that DNAnexus file IDs are returned
+        """
+        filename = "file-1234"
+        mock_download.return_value = filename
+        vcf, index = download_clinvar_dnanexus(
+            "https://ftp.ncbi.nlm.nih.gov",
+            "/pub/clinvar/vcf_GRCh38/weekly/",
+            "", "/my_folder", "my_file.vcf.gz", "my_checksum.md5",
+            "my_file.vcf.gz.tbi"
+        )
+        with self.subTest():
+            assert vcf == filename
+        with self.subTest():
+            assert index == filename
 
 
 if __name__ == "__main__":
